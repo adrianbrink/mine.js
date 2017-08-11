427fbd008a59b8fe6fb04b7c2043a8965dc9ec07
anoff <offenhaeuser@gmail.com>
2017-08-12 01:24:40 +0200

    :star2: feat: stores data to IPFS

--- app.js
+++ app.js
@@ -13,17 +13,17 @@ const fs = require('fs')
 const spawn = require('child_process').spawn
 
 // magic numbers that need to be parsed via CLI
-const contractAddress = '0x9c625c13048a5f5a374acc4ed6801211020f212a'
-const mineAddress = '0x7d372bBA2139adfe8f4D68dA91B9f0Ea4dB1aef0' // 2nd account
-
-const sonar = new Sonar(contractAddress, mineAddress)
-console.log('reading accounts')
-sonar.web3.eth.getAccounts()
-  .then(a => console.log(a.slice(0, 10)))
+const contractAddress = '0x656385f6914469fff55c34299258ce965aa134bf'
+const mineAddress = '0xb8CE9ab6943e0eCED004cDe8e3bBed6568B2Fa01' // additional account w/ funds
 
 const ipfs = ipfsAPI('localhost', '5001', {protocol: 'http'})
 
 async function checkForModels () {
+  const sonar = new Sonar(contractAddress, mineAddress)
+  console.log('reading accounts')
+  sonar.web3.eth.getAccounts()
+    .then(a => console.log(a.slice(0, 10)))
+
   const modelCount = await sonar.getNumModels()
   console.log(`${modelCount} models found`)
 
@@ -62,7 +62,7 @@ async function checkForModels () {
     const childOpts = {
       shell: true,
       stdio: config.debug ? 'inherit' : ['ignore', 'ignore', process.stderr],
-      cwd: '/developer/anoff/openmined-syft'
+      cwd: '/developer/anoff/openmined-syft' // TODO: this should NOT be absolute
     }
     const sp = spawn(`syft_cmd generate_gradient`, [`-model ${tmpPaths.model}`, `-input_data ${path.join(__dirname, 'data/mine/diabetes/diabetes_input.csv')}`, `-target_data ${path.join(__dirname, 'data/mine/diabetes/diabetes_output.csv')}`, `-gradient ${tmpPaths.gradient}`], childOpts)
     await new Promise((resolve, reject) => {
@@ -83,7 +83,7 @@ async function checkForModels () {
       }]
 
       ipfs.files.add(files, (err, res) => {
-        if (err) console.error(err)
+        if (err) return console.error(err)
         const obj = res.find(e => e.path === tmpPaths.gradient)
         resolve(obj.hash)
       })
@@ -96,3 +96,10 @@ async function checkForModels () {
 }
 
 checkForModels()
+
+/*
+0x516d51316142636f727a6e53364a61524d735a7a4273524c756a57563846464b, 0x4b57707854596e48644266327270000000000000000000000000000000000000
+'QmQ1aBcorznS6JaRMsZzBsRLujWV8FFK', 'KWpxTYnHdBf2rp\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+
+'QmQ1aBcorznS6JaRMsZzBsRLujWV8FFKKWpxTYnHdBf2rp'
+*/
